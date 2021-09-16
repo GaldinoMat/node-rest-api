73517d55e419a447dabab989dd2af178561bafb1
GaldinoMat <51252943+GaldinoMat@users.noreply.github.com>
2021-09-16 13:49:59 -0300

    Create sequelize connection with DB using mocked data.

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
+import dotenv from "dotenv";
 import express from "express";
 import home from "./src/routes/home";
+import "./src/database";
+
+dotenv.config();
 
 class App {
   constructor() {
--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Student from "../models/Student";
+
 class Home {
-  index(req, res) {
-    res.json({
-      testBool: true,
+  async index(req, res) {
+    const newStudent = await Student.create({
+      name: "Mateus",
+      surname: "Galdino",
+      email: "mateusgaldino1000@hotmail.com",
+      age: 26,
+      weight: 77,
+      height: 1.84,
     });
+    res.json(newStudent);
   }
 }
 
