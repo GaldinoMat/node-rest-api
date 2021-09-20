02eb8daad727142f2460f0b63ed823e80e9fa8f5
GaldinoMat <51252943+GaldinoMat@users.noreply.github.com>
2021-09-20 18:26:14 -0300

    Implement file extension validation

--- src/routes/photo.js
+++ src/routes/photo.js
@@ -1,13 +1,10 @@
 import { Router } from "express";
-import multer from "multer";
-import multerConfig from "../config/multerConfig";
+
 import PhotoController from "../controllers/PhotoController";
 
 const router = new Router();
 
-const upload = multer(multerConfig);
-
 // Sets route http actions
-router.post("/", upload.single("photo"), PhotoController.store);
+router.post("/", PhotoController.store);
 
 export default router;
