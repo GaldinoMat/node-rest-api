77bd74aa979a0cbd9a78fde9532545aa6f451558
GaldinoMat <51252943+GaldinoMat@users.noreply.github.com>
2021-10-02 13:23:47 -0300

    Install cors and helmet dependencies to avoid cors errors

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
 import { resolve } from "path";
 import home from "./src/routes/home";
 import user from "./src/routes/user";
@@ -10,6 +12,18 @@ import "./src/database";
 
 dotenv.config();
 
+const whiteList = ["https://react.mydomain.com.br", "http://localhost:3000"];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not Allowed by Cors"));
+    }
+  },
+};
+
 class App {
   constructor() {
     this.app = express();
@@ -19,6 +33,8 @@ class App {
 
   // Middleares used
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, "uploads")));
